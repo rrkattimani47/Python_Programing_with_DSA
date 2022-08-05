97fcb61e1d441e14665ff9d918022e1233d3df4d
Rashmi Kattimani <rrkattimani47@gmail.com>
2022-08-05 19:44:40 +0530

    Mirror number pattern

---
+++ Patterns/MirrorNumberPattern.py
@@ -0,0 +1,46 @@
+"""
+Print the following pattern for the given N number of rows.
+Pattern for N = 4
+
+
+
+
+The dots represent spaces.
+
+
+Input format :
+Integer N (Total no. of rows)
+Output format :
+Pattern in N lines
+Constraints
+0 <= N <= 50
+Sample Input 1:
+3
+Sample Output 1:
+      1 
+    12
+  123
+Sample Input 2:
+4
+Sample Output 2:
+      1 
+    12
+  123
+1234
+"""
+
+## Read input as specified in the question
+## Print the required output in given format
+n=int(input())
+i=1
+while(i<=n):
+    space=1
+    while(space<=n-i):
+        print(" ",end="")
+        space=space+1
+    j=1
+    while(j<=i):
+        print(j,end="")
+        j=j+1
+    print()
+    i=i+1
