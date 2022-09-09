f3d0a4b8ffafd80cb4cb2cbbe0cb9cf7a3c33242
Rashmi Kattimani <rrkattimani47@gmail.com>
2022-09-09 16:32:51 +0530

    Create CheckPalindrome.py

---
+++ Recursion/CheckPalindrome.py
@@ -0,0 +1,33 @@
+"""
+Check whether a given String S is a palindrome using recursion. Return true or false.
+Input Format :
+String S
+Output Format :
+'true' or 'false'
+Constraints :
+0 <= |S| <= 1000
+where |S| represents length of string S.
+Sample Input 1 :
+racecar
+Sample Output 1:
+true
+Sample Input 2 :
+ninja
+Sample Output 2:
+false
+"""
+## Read input as specified in the question.
+## Print output as specified in the question.
+def is_palindrome(s):
+    if len(s)<1:
+        return True
+    else:
+        if s[0]==s[-1]:
+            return is_palindrome(s[1:-1])
+        else:
+            return False
+a=input()
+if (is_palindrome(a)==True):
+    print("true")
+else:
+    print("false")
